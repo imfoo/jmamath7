0957bea1657f2f87c3fd00654326239ee587fd49
cld <cld@localhost.localdomain>
2017-11-10 18:50:27 -0500

    adding test file to check permissions on creation

--- warmups/future/convert_to_png.sh
+++ warmups/future/convert_to_png.sh
@@ -1,4 +1,4 @@
-for page in $(seq 1 120); do
+for i in $(seq 1 120); do
     warmupno=`printf "%03d" $page`
     convert -density 300 -quality 85 7thGradeMathWarmUp${warmupno}.pdf 7thGradeMathWarmUp${warmupno}.png
 done
